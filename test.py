251a89c1b7bd9ad36d272b71b609688dcd1b6ff2
周城成 <zccplot@gmail.com>
2018-11-29 12:03:30 +0800

    修改bug

--- test.py
+++ test.py
@@ -1,20 +1,13 @@
 # __Author__:Zcc
 from chi_merge import ChiMerge
-from util import convert_col_index
-import pandas as pd
+from sklearn.datasets import load_iris
 
-X = pd.read_csv('X.csv')
-y = pd.read_csv('y.csv', header=None)
-num_features = ['int_rate', 'emp_length', 'annual_inc', 'dti', 'delinq_2yrs', 'earliest_cr_to_app', 'inq_last_6mths',
-                'mths_since_last_record', 'mths_since_last_delinq', 'open_acc', 'pub_rec', 'total_acc', 'limit_income']
+iris = load_iris()
 
-cat_features = ['home_ownership', 'verification_status', 'desc', 'purpose', 'zip_code', 'addr_state', 'pub_rec_bankruptcies']
-num_feature_index = convert_col_index(X, num_features)
-cat_features_index = convert_col_index(X, cat_features)
-cm = ChiMerge(num_features=num_feature_index, cat_features=cat_features_index, special_value=-1, max_bin=20)
-
-cm.fit(X, y)
-X_merge = cm.transform(X)
+cm = ChiMerge(num_features=[0, 1, 2, 3], cat_features=[])
+cm.fit(iris.data, iris.target)
+cm.transform(iris.data)
+print(cm.split_point)
 
 
 
